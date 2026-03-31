b533200f404bd61d89d0e1baae21138f5e23ecae
Neighbor-Z <zerkam04@gmail.com>
2026-03-31 20:50:15 +0800

    Update __version__.py

--- __version__.py
+++ __version__.py
@@ -1,6 +1,6 @@
 
-__version__ = "1.6"
-__build__ = "20260316"
+__version__ = "1.6.1"
+__build__ = "20260331"
 __author__ = "Neighborhood-of-Z"
 __author2__ = "Mainz0412"
 __author2_email__ = "czy-050412@outlook.com"
